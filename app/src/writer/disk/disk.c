5e41a0af94b960b26f414451d5c62789104a41df
Tomas Koranda <tom.koranda@gmail.com>
2022-03-02 10:38:31 +0100

    Amend: api for writing to disk

---
+++ app/src/writer/disk/disk.c
@@ -0,0 +1,205 @@
+#include "disk.h"
+
+/* -------------------------------------------------------------------------- */
+
+static struct fs_file_t file;
+static const char *disk_mount_pt = "/SD:";
+
+/* mounting info */
+static FATFS fat_fs;
+static struct fs_mount_t mp = {
+    .type = FS_FATFS,
+    .fs_data = &fat_fs,
+};
+
+/* -------------------------------------------------------------------------- */
+
+int mount_disk()
+{
+    mp.mnt_point = disk_mount_pt;
+    int res = fs_mount(&mp);
+
+    if (res == FR_OK)
+    {
+        printk("Disk mounted. res=%d\n", res);
+    }
+    else
+    {
+        printk("Error mounting disk. res=%d\n", res);
+    }
+    return -res;
+}
+
+int unmount_disk()
+{
+    mp.mnt_point = disk_mount_pt;
+    int res = fs_unmount(&mp);
+    if (res == 0)
+    {
+        printk("Disk unmounted. res=%d\n", res);
+    }
+    else
+    {
+        printk("Error unmounting disk. res=%d\n", res);
+    }
+    return -res;
+}
+
+/* -------------------------------------------------------------------------- */
+
+int test_disk_config()
+{
+    /* raw disk i/o */
+    static const char *disk_pdrv = "SD";
+    uint64_t memory_size_mb;
+    uint32_t block_count;
+    uint32_t block_size;
+
+    int err = 0;
+
+    err = disk_access_init(disk_pdrv);
+    if (err)
+    {
+        printk("Storage init ERROR!\n");
+        return err;
+    }
+
+    err = disk_access_ioctl(disk_pdrv,
+                            DISK_IOCTL_GET_SECTOR_COUNT, &block_count);
+    if (err)
+    {
+        printk("Unable to get sector count\n");
+        return err;
+    }
+
+    printk("Block count %u", block_count);
+
+    err = disk_access_ioctl(disk_pdrv,
+                            DISK_IOCTL_GET_SECTOR_SIZE, &block_size);
+    if (err)
+    {
+        printk("Unable to get sector size\n");
+        return err;
+    }
+
+    printk("Sector size %u\n", block_size);
+
+    memory_size_mb = (uint64_t)block_count * block_size;
+    printk("Memory Size(MB) %u\n", (uint32_t)(memory_size_mb >> 20));
+
+    return 0;
+}
+
+/* -------------------------------------------------------------------------- */
+
+int list_dir(const char *path)
+{
+    // TODO: pass by ref list of all files/dirs
+    int res;
+    struct fs_dir_t dirp;
+    static struct fs_dirent entry;
+
+    fs_dir_t_init(&dirp);
+
+    /* Verify fs_opendir() */
+    res = fs_opendir(&dirp, path);
+    if (res)
+    {
+        printk("Error opening dir %s [%d]\n", path, res);
+        return res;
+    }
+
+    printk("\nListing dir %s ...\n", path);
+    for (;;)
+    {
+        /* Verify fs_readdir() */
+        res = fs_readdir(&dirp, &entry);
+        if (res)
+        {
+            printk("Error reading dir %s [%d]\n", path, res);
+            break;
+        }
+
+        /* entry.name[0] == 0 means end-of-dir */
+        if (entry.name[0] == 0)
+        {
+            break;
+        }
+
+        if (entry.type == FS_DIR_ENTRY_DIR)
+        {
+            printk("[DIR ] %s\n", entry.name);
+        }
+        else
+        {
+            printk("[FILE] %s (size = %zu)\n",
+                   entry.name, entry.size);
+        }
+    }
+
+    /* Verify fs_closedir() */
+    res = fs_closedir(&dirp);
+    if (res)
+    {
+        printk("Error closing dir %s [%d]\n", path, res);
+    }
+
+    return res;
+}
+
+/* -------------------------------------------------------------------------- */
+
+int write(const char *path, const void *buf, size_t size);
+
+void test_write()
+{
+    const char *path = DISK_MOUNT_PT "/" "test.csv";
+    char out_str[100];
+    int written;
+
+    for (int i = 0; i < 32; i++)
+    {
+        written = snprintf(out_str, sizeof(out_str) - 1, "%.6lf, %.6lf, %.6lf\r\n",
+                           i * 1.0, i * 2.0, i * 3.0);
+
+        // TODO: ineffective, (avoid opening and closing file)
+        write(path, out_str, written);
+    }
+}
+/* -------------------------------------------------------------------------- */
+
+int write(const char *path, const void *buf, size_t size)
+{
+    // TODO: diff function for multiple sequential writes (avoid opening and closing file)
+    // TODO: select append or not
+    fs_mode_t flags = FS_O_WRITE | FS_O_CREATE | FS_O_APPEND;
+    int res = 0;
+
+    // TODO: once is enough, during mount?
+    fs_file_t_init(&file);
+
+    res = fs_open(&file, path, flags);
+    if (res != 0)
+    {
+        printk("file not created. err=%d\n", res);
+        return res;
+    }
+
+    int w_size = fs_write(&file, buf, size);
+    if (w_size > 0)
+    {
+        printk("%d bytes written\r\n", w_size);
+    }
+    else
+    {
+        printk("err in writing. err=%d\n", w_size);
+    }
+
+    res = fs_close(&file);
+    if (res != 0)
+    {
+        printk("file not closed. err=%d\n", res);
+        return res;
+    }
+    return w_size;
+}
---
+++ app/src/writer/disk/disk.h
@@ -0,0 +1,79 @@
+#ifndef __DISK_H__
+#define __DISK_H__
+
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <device.h>
+
+#include <storage/disk_access.h>
+#include <fs/fs.h>
+#include <ff.h>
+
+#include <stdio.h>
+
+/* -------------------------------------------------------------------------- */
+
+#define DISK_MOUNT_PT "/SD:"
+// #define MAKE_DISK_PATH(P) DISK_MOUNT_PT "/" P
+// extern const char *disk_mount_pt;
+
+/* -------------------------------------------------------------------------- */
+
+/**
+ * @brief  mount disk wrapper
+ * @retval 0 on success;
+ * @retval <0 on error.
+ */
+int mount_disk();
+
+/**
+ * @brief  unmount disk wrapper
+ * @retval 0 on success;
+ * @retval <0 on error.
+ */
+int unmount_disk();
+
+/* -------------------------------------------------------------------------- */
+
+/**
+ * @brief  read disk config
+ * @retval 0 on success;
+ * @retval <0 on error.
+ */
+int test_disk_config();
+
+/* -------------------------------------------------------------------------- */
+/**
+ * @brief List contents (dirs, files) of a directory including sizes
+ * @param path Path to the file or directory
+ * @retval 0 on success;
+ * @retval <0 on error.
+ */
+int list_dir(const char *path);
+/* -------------------------------------------------------------------------- */
+/**
+ * @brief List contents (dirs, files) of a directory including sizes
+ * @param path Path to the file or directory
+ * @retval 0 on success;
+ * @retval <0 on error.
+ */
+int list_dir(const char *path);
+/* -------------------------------------------------------------------------- */
+/**
+ * @brief  write to a file (create if non-existant)
+ * @param path Path to the file
+ * @param buf  Buffer with data
+ * @param size number of bytes to write
+ * @retval >0 number of bytes written
+ * @retval <0 on error.
+ */
+int write(const char *path, const void *buf, size_t size);
+
+/* -------------------------------------------------------------------------- */
+/**
+ * @brief List contents (dirs, files) of a directory including sizes
+ */
+void test_write();
+/* -------------------------------------------------------------------------- */
+
+#endif // __DISK_H__
