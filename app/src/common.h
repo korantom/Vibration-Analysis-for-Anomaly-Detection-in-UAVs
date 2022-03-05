81395386681171a3cd5c1a957d94d1b052469ef0
Tomas Koranda <tom.koranda@gmail.com>
2022-03-05 14:45:58 +0100

    feat: Add SYS_INIT to main
    - SYS_INIT macro runs a function at system boot

--- app/src/common.h
+++ app/src/common.h
@@ -1,4 +1,7 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+// SYS_INIT priorities: integer in range [0-99], lower values indicate earlier initialization
+#define MAIN_SYS_INIT_PRIORITY 31
+
 #endif // __COMMON_H__
--- app/src/main.c
+++ app/src/main.c
@@ -8,5 +8,15 @@
 
 void main(void)
 {
-    printk("Hello world\n");
+    printk("Main: Hello world\n");
 }
+
+/* -------------------------------------------------------------------------- */
+
+static int _init_main(const struct device *_)
+{
+    printk("main.c thread: %p, SYS_INIT priority: %d\n", k_current_get(), MAIN_SYS_INIT_PRIORITY);
+    return 0;
+}
+
+SYS_INIT(_init_main, APPLICATION, MAIN_SYS_INIT_PRIORITY);
