72ba2d246ed98073d23b88064b37cdbde181a599
Tomas Koranda <tom.koranda@gmail.com>
2022-03-05 14:15:15 +0100

    Hello world (init app)
    - add main.c
    - add common.h
      - will hold global shared constants, ...
    
    
    - add cmake
    
    - add kconfig (Kernel configuration file)
      - specifies kernel configuration options (merged with board-specific settings to produce a kernel configuration)
    
    - add overlay file
      - type of a devicetree input file (https://docs.zephyrproject.org/latest/guides/dts/intro.html#devicetree-intro)
      - overlays adapt the base devicetree for different purposes:
        - enable a peripheral disabled by default
        - select a sensor on the board for an application specific purpose
        - ...

---
+++ app/src/main.c
@@ -0,0 +1,12 @@
+#include <device.h>
+#include <sys/printk.h>
+#include <zephyr.h>
+
+#include "common.h"
+
+/* -------------------------------------------------------------------------- */
+
+void main(void)
+{
+    printk("Hello world\n");
+}
