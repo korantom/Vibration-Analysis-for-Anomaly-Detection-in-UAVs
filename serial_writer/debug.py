1ca2057068db07c0fe53abd61a3ff2d78c9d63aa
Tomas Koranda <tom.koranda@gmail.com>
2022-04-04 16:53:38 +0200

    Add simple python communication

--- serial_writer/debug.py
+++ serial_writer/debug.py
@@ -2,25 +2,65 @@ import serial
 import time
 
 # Open serial port
-ser = serial.Serial("/dev/cu.usbmodemC1E9B7D03", baudrate=115200, timeout=1.0)
+ser = serial.Serial(
+    "/dev/cu.usbmodemC1E9B7D03",
+    # baudrate=115200,
+    baudrate=9600,
+    timeout=0.5,
+    # Number of data bits
+    bytesize=serial.EIGHTBITS,
+    # bytesize=serial.SEVENBITS,
+    # Parity check
+    # parity=serial.PARITY_NONE,
+    # parity=serial.PARITY_ODD,
+    # parity=serial.PARITY_EVEN,
+    # Stop bits
+    # stopbits=serial.STOPBITS_ONE,
+    # stopbits=serial.STOPBITS_ONE_POINT_FIVE,
+    # stopbits=serial.STOPBITS_TWO,
+)
 time.sleep(2)
 
-# Start an infinite print loop
-while input("start tester_infinite_print? (y/n): ") != "y":
-    pass
-ser.write("tester_infinite_print\r".encode("ascii"))
+
+def check_shell_prompt(line: str):
+    return "uart:~$" in line
+
+
+print(f"{ser=}")
 
 while True:
-    time.sleep(1)
+    # time.sleep(1)
 
-    # Check how many bytes in buffer (doesn't wait)
+    # Check how many bytes in buffer (at given moment, doesn't wait)
     buffered_byte_count = ser.in_waiting
-    print(f"{buffered_byte_count=}")
 
-    # Read a '\n' terminated line, timeouts otherwise
+    # Read a '\n' terminated line (timeouts)
     line = ser.readline()
-    print(f"{type(line), line=}")
+
+    # Skip if nothing read
+    if line == bytes():
+        continue
+
+    # Try to decode read line
+    try:
+        line_str = line.decode("utf-8")
+        print(line_str)
+
+    except:
+        line_str = ""
+        print(f"unable to decode ascii")
+        print(line)
     print()
+
+    # Check line for prompt
+    if check_shell_prompt(line_str):
+
+        # Wait for input
+        while (cmd := input("uart:~$")) == "":
+            pass
+
+        cmd += "\r\n"
+        ser.write(cmd.encode("ascii"))
 
 
 # close port
